e62103d643664ebf88d24580f52404cec0075ac8
thesadie <sgill@flowwest.com>
2017-10-23 15:47:38 -0700

    organized modeling exist

--- data-raw/mark_gard_data/modeling.R
+++ data-raw/mark_gard_data/modeling.R
@@ -5,7 +5,7 @@ library(readxl)
 fall_run_fp <- read_excel('data-raw/mark_gard_data/floodplain2017update.xlsx')
 
 fp <- fall_run_fp %>%
-  mutate(Floodplain_Modeled = Method == 'redo scaling, length based and then categorize by geography') %>%
+  mutate(Floodplain_Modeled = Method != 'redo scaling, length based and then categorize by geography') %>%
   select(Order, Watershed, Floodplain_Modeled)
 
 fall_run_ic <- read_excel('data-raw/mark_gard_data/instream2017update.xlsx', skip = 1)
@@ -25,10 +25,19 @@ icsr <- fall_run_ic %>%
   mutate(Instream_SR_Modeled = Method != 'Total area * percent suitable') %>%
   select(Watershed, Instream_SR_Modeled)
 
-fp %>%
+modeling_exist <- fp %>%
   left_join(icr) %>%
   left_join(ics) %>%
-  left_join(icsr) %>% View()
-  mutate(Instream_S = ifelse(is.na(Instream_Spawning_Modeled), Instream_SR_Modeled, Instream_Spawning_Modeled))
+  left_join(icsr) %>%
+  mutate(Instream_Spawning_Modeled = ifelse(is.na(Instream_Spawning_Modeled),
+                             Instream_SR_Modeled, Instream_Spawning_Modeled),
+         Instream_Rearing_Modeled = ifelse(is.na(Instream_Rearing_Modeled),
+                             Instream_SR_Modeled, Instream_Rearing_Modeled)) %>%
+  select(Order, Watershed, Spawning = Instream_Spawning_Modeled,
+         Rearing = Instream_Rearing_Modeled, Floodplain = Floodplain_Modeled) %>%
+  filter(Watershed != 'Bear Creek') %>%
+  bind_rows(tibble(Order = 4, Watershed = 'Bear Creek', Spawning = FALSE, Rearing = FALSE, Floodplain = FALSE)) %>%
+  mutate(Rearing = replace(Rearing, Watershed == 'South Delta', FALSE)) %>%
+  arrange(Order) %>%
+  unique()
 
-View(fall_run_ic)
