bffb5f4d3d3f7c70b5c0f8573a42854ddce4475c
thesadie <sgill@flowwest.com>
2017-10-31 16:50:52 -0700

    rene's fp logic

---
+++ data-raw/floodplain_inundation_thresholds/rene_henery_scoring_logic.R
@@ -0,0 +1,7 @@
+# From Rene Henery
+# For consistency with the CVHE The logic should be as follows
+# (note that I included the scorable days in parentheses to help clarify):
+# 1 week:   3 days (days 1-3) growth, 7 days (days 1-7) survival
+# 2 weeks: 8 days (days 1-3; 10-14) growth, 14 days (days 1-14) survival
+# 3 weeks: 15 days (days 1-3; 10-21) growth , 21 days (days 1-21) survival
+# 4 weeks: 15 days(same as above)  growth, 28 (30?) days (days 1-x) survival
