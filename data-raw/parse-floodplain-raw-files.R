107d74bbd5b40d17c68f69228ee4068657736973
ergz <erodriguez@flowwest.com>
2017-11-03 11:56:19 -0700

    started tackling issue #9

--- data-raw/parse-floodplain-raw-files.R
+++ data-raw/parse-floodplain-raw-files.R
@@ -14,3 +14,128 @@ american_river_floodplain <- american_river %>%
 
 devtools::use_data(american_river_floodplain, overwrite = TRUE)
 
+# bear river
+bear_river <- read_csv("data-raw/bear_river_floodplain.csv")
+
+bear_river_floodplain <- bear_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(bear_river_floodplain, overwrite = TRUE)
+
+# big chico
+big_chico <- read_csv("data-raw/big_chico_creek_floodplain.csv")
+
+big_chico_creek_floodplain <- big_chico %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(big_chico_creek_floodplain, overwrite = TRUE)
+
+# butte creek
+butte_creek <- read_csv("data-raw/butte_creek_floodplain.csv")
+
+butte_creek_floodplain <- butte_creek %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(butte_creek_floodplain, overwrite = TRUE)
+
+# calaveras
+calaveras_river <- read_csv("data-raw/calaveras_river_floodplain.csv")
+
+calaveras_river_floodplain <- calaveras_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(calaveras_river_floodplain, overwrite = TRUE)
+
+# consumnes river
+cosumnes_river <- read_csv("data-raw/cosumnes_river_floodplain.csv")
+
+cosumnes_river_floodplain <- cosumnes_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(cosumnes_river_floodplain, overwrite = TRUE)
+
+# cottonwood creek
+cottonwood_creek <- read_csv("data-raw/cottonwood_creek_floodplain.csv")
+
+cottonwood_creek_floodplain <- cottonwood_creek %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(cottonwood_creek_floodplain, overwrite = TRUE)
+
+# deer creek
+deer_creek <- read_csv("data-raw/deer_creek_floodplain.csv")
+
+deer_creek_floodplain <- deer_creek %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(deer_creek_floodplain, overwrite = TRUE)
+
+# elder creek
+elder_creek <- read_csv("data-raw/elder_creek_floodplain.csv")
+
+elder_creek_floodplain <- elder_creek %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(elder_creek_floodplain, overwrite = TRUE)
+
+# feather river
+feather_river <- read_csv("data-raw/feather_river_floodplain.csv")
+
+feather_river_floodplain <- feather_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(feather_river_floodplain, overwrite = TRUE)
+
+# merced river
+merced_river <- read_csv("data-raw/merced_river_floodplain.csv")
+
+merced_river_floodplain <- merced_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(merced_river_floodplain, overwrite = TRUE)
+
+# moke
+mokelumne_river <- read_csv("data-raw/mokelumne_river_floodplain.csv")
+
+mokelumne_river_floodplain <- mokelumne_river %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(mokelumne_river_floodplain, overwrite = TRUE)
+
+# north delta
+north_delta <- read_csv("data-raw/north_delta_floodplain.csv")
+
+north_delta_floodplain <- north_delta %>%
+  select(flow_cfs,
+         FR_floodplain_acres = floodplain_acres,
+         watershed)
+
+devtools::use_data(north_delta_floodplain, overwrite = TRUE)
+
+
+
+
+
+
