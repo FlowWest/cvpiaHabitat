17e9801e32290f0dbbb174f97af1e75d9a9f5507
thesadie <sgill@flowwest.com>
2018-01-16 16:30:39 -0800

    bear river inundated

--- data-raw/floodplain_inundation_thresholds/create_inundation_table.R
+++ data-raw/floodplain_inundation_thresholds/create_inundation_table.R
@@ -5,6 +5,7 @@ weeks_inundated <- bind_rows(
   read_rds('data-raw/floodplain_inundation_thresholds/big_chico_inundated.rds'),
   read_rds('data-raw/floodplain_inundation_thresholds/butte_inundated.rds'),
   read_rds('data-raw/floodplain_inundation_thresholds/cottonwood_inundated.rds'),
-  read_rds('data-raw/floodplain_inundation_thresholds/deer_inundated.rds'))
+  read_rds('data-raw/floodplain_inundation_thresholds/deer_inundated.rds'),
+  read_rds('data-raw/floodplain_inundation_thresholds/bear_river_inundated.rds'))
 
 use_data(weeks_inundated, overwrite = TRUE)
