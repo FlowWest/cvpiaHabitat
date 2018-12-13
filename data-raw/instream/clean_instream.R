b2a831ba5f63812491cb9ebecd579e8d022c3c0a
sadie <sgill@flowwest.com>
2018-12-13 12:43:58 -0800

    refactor butte creek hab

--- data-raw/instream/clean_instream.R
+++ data-raw/instream/clean_instream.R
@@ -12,6 +12,7 @@ library(readxl)
 # Cottonwood Creek see 'data-raw/instream/cottonwood'
 # Battle Creek see 'data-raw/instream/battle'
 # Bear River see 'data-raw/instream/bear_river'
+# Butte Creek see 'data-raw/instream/butte'
 
 # delta
 delta <- read_csv('data-raw/instream/north_delta_instream.csv', skip = 1)
@@ -45,18 +46,6 @@ use_data(south_delta_percent_suitability)
 
 # this portion cleans up the naming conventions -------------------------------------
 # naming convention: SPECIES_LIFESTAGE_UNITS for each column
-
-# butte creek
-butte_creek <- read_csv("data-raw/instream/butte_creek_instream.csv", skip=1)
-butte_creek_instream <- butte_creek %>%
-  select(flow_cfs,
-         FR_spawn_wua = spawn_WUA,
-         FR_fry_wua = fry_WUA,
-         FR_juv_wua = juv_WUA,
-         adult_trout_WUA,
-         watershed)
-
-devtools::use_data(butte_creek_instream, overwrite = TRUE)
 
 # calaveras river
 calaveras_river <- read_csv("data-raw/instream/calaveras_river_instream.csv", skip=2)
