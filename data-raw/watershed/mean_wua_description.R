88614d9f4faa6c07a9d9bad1bfa3cce16cd336fc
ErinCain <erincain@berkeley.edu>
2019-08-05 12:28:31 -0700

    thomes creek with mean wua description finished and minor edits to mean wua description function

--- data-raw/watershed/mean_wua_description.R
+++ data-raw/watershed/mean_wua_description.R
@@ -21,20 +21,20 @@ print_mean_wua_details <- function(ws) {
   name <- ws
   return(
     glue("No watershed specific salmonid habitat data was available for {name}.",
-         "A regional weighted usable area (WUA) relationship with flow",
-         "was derived for {name} by averaging the WUA values on Battle Creek,",
-         "Butte Creek, Clear Creek, Cottonwood Creek and Cow Creek. The ",
+         " A regional weighted usable area (WUA) relationship with flow",
+         " was derived for {name} by averaging the WUA values on Battle Creek,",
+         " Butte Creek, Clear Creek, Cottonwood Creek and Cow Creek. The ",
          "geomorphic and hydrologic conditions in {name} (watershed area =",
          " {area} sqkm, active channel width = {channel_width} meters)",
-         "are similar to those on ",
+         " are similar to those on ",
          "Battle Creek (watershed area = 957 sqkm; active channel width = 22.5 m",
          "), Butte Creek (watershed area = 2123 sqkm; active channel width = 15.2 m",
-         "); Clear Creek (watershed area = 645 sqkm; active channel ",
+         "), Clear Creek (watershed area = 645 sqkm; active channel ",
          "width = 18 m), Cottonwood Creek (watershed area = 2444 sqkm;",
-         "active channel width = 28.25 m), and Cow Creek (watershed",
-         "area = 1107 sqkm; active channel width = 8.5 m). The regional WUA",
-         "relationships for Bear Creek were multiplied by the length of spawning",
-         "and rearing extents mapped by the Science Integration Team (SIT)."
+         " active channel width = 28.25 m), and Cow Creek (watershed",
+         " area = 1107 sqkm; active channel width = 8.5 m). The regional WUA",
+         " relationships for Bear Creek were multiplied by the length of spawning",
+         " and rearing extents mapped by the Science Integration Team (SIT)."
     )
   )
 }
