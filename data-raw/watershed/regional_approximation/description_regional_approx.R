0a392b03e2ae56056203bf9199d874b901f71970
ErinCain <erincain@berkeley.edu>
2019-08-14 15:51:09 -0700

    Fix description of regional approx function with edits from sadi

--- data-raw/watershed/regional_approximation/description_regional_approx.R
+++ data-raw/watershed/regional_approximation/description_regional_approx.R
@@ -2,15 +2,14 @@ library(tidyverse)
 library(readxl)
 library(glue)
 
-print_regional_approx <- function(ws) {
-  watershed_name <- ws
+print_regional_approx <- function(watershed_name) {
 
   return(
-    glue( " No watershed specific instream spawning or rearing habitat data was",
-          " available for {watershed_name}. Instream spawning and rearing data for",
-          " {watershed_name} to use in the DSM was developed using a regional",
-          " approximation for similar watersheds in the Upper-mid Sacramento River",
-          " Region with habitat modeling." )
+    glue( ' No watershed specific instream spawning or rearing habitat data was',
+          ' available for {watershed_name}. Instream spawning and rearing data for',
+          ' {watershed_name} to use in the DSM was developed using a regional',
+          ' approximation for similar watersheds in the Upper-mid Sacramento River',
+          ' Region with habitat modeling.' )
     )
 }
 
