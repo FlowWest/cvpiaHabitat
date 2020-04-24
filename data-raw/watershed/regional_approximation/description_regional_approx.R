57e3fde74e5d38ba9e68a7b72c578ef9cf954346
Isabelle Kavanagh <ikavanagh@flowwest.com>
2020-04-24 16:54:33 -0700

    mostly changing regional approx link

--- data-raw/watershed/regional_approximation/description_regional_approx.R
+++ data-raw/watershed/regional_approximation/description_regional_approx.R
@@ -7,8 +7,8 @@ print_regional_approx <- function(watershed_name) {
   return(
     glue( ' No watershed specific instream spawning or rearing habitat data was',
           ' available for {watershed_name}. Instream spawning and rearing data for',
-          ' {watershed_name} to use in the DSM was developed using a regional',
-          ' approximation for similar watersheds in the Upper-mid Sacramento River',
+          ' {watershed_name} to use in the DSM was developed using a [regional',
+          ' approximation](http://cvpia-habitat-docs-markdown.s3-website-us-west-2.amazonaws.com/watershed/Regional_Approximation.html) for similar watersheds in the Upper-mid Sacramento River',
           ' Region with habitat modeling.' )
     )
 }
