6adfda97f5034b28c262f7199b7ec89bd2c43557
Isabelle Kavanagh <ikavanagh@flowwest.com>
2020-07-30 17:08:30 -0700

    starting testing edits for american river

--- tests/testthat/test-american-river.R
+++ tests/testthat/test-american-river.R
@@ -4,12 +4,15 @@ context('American River Habitat')
 test_that('FR fry American River works', {
   # flow 300
   wua <- cvpiaHabitat::american_river_instream$FR_fry_wua[6]
-  stream_length <- cvpiaHabitat::watershed_lengths[[40, 5]]
+  stream_length <- cvpiaHabitat::watershed_lengths[cvpiaHabitat::watershed_lengths$watershed == "American River" &
+                                                     cvpiaHabitat::watershed_lengths$lifestage == "rearing",
+                                                   "feet"]
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  x <- (((stream_length/1000) * wua)/10.7639)[1,1] # HAVING TROUBLE HERE WITH TYPE
 
+  flow <- cvpiaHabitat::american_river_instream$flow_cfs[6]
   expect_equal(
-    set_instream_habitat('American River', 'fr', 'fry', 300), x)
+    set_instream_habitat('American River', 'fr', 'fry', flow), x)
 
 })
 
