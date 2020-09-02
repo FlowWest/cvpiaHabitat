37098545324c93d63e44991d68f50dc892c648f6
Isabelle Kavanagh <ikavanagh@flowwest.com>
2020-09-01 17:55:02 -0700

    butte creek etesting edits

--- tests/testthat/test-butte-creek.R
+++ tests/testthat/test-butte-creek.R
@@ -1,148 +1,160 @@
 library(cvpiaHabitat)
 context('Butte Creek Habitat')
 
-test_that('FR fry Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'fr', 'feet'])
+test_that("modeling of species coverage hasn't changed since v2.0 - Butte", {
+  modeling <- subset(cvpiaHabitat::modeling_exist, Watershed == 'Butte Creek')
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  expect_equal(modeling$FR_spawn, TRUE)
+  expect_equal(modeling$FR_fry, TRUE)
+  expect_equal(modeling$FR_juv, TRUE)
+  expect_equal(modeling$FR_floodplain, TRUE)
 
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_instream_habitat('Butte Creek', 'fr', 'fry', flow), x)
+  expect_equal(modeling$SR_spawn, FALSE)
+  expect_equal(modeling$SR_fry, FALSE)
+  expect_equal(modeling$SR_juv, FALSE)
+  expect_equal(modeling$SR_floodplain, TRUE)
 
+  expect_equal(modeling$ST_spawn, FALSE)
+  expect_equal(modeling$ST_fry, FALSE)
+  expect_equal(modeling$ST_juv, FALSE)
+  expect_equal(modeling$ST_floodplain, TRUE)
+  expect_equal(modeling$ST_adult, FALSE)
 })
 
-test_that('FR juv Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'fr', 'feet'])
+test_that('FR instream Bear River works', {
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_juv_wua))[1]
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_spawn_wua))[1]
 
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
+  fry_wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[juv_not_na_index]
+  spawn_wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[spawn_not_na_index]
+
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'Butte Creek' & lifestage == 'rearing'
+                                  & species == 'fr')$feet
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Butte Creek' & lifestage == 'spawning'
+                                   & species == 'fr')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[juv_not_na_index]
+  spawn_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[spawn_not_na_index]
+
   expect_equal(
-    set_instream_habitat('Butte Creek', 'fr', 'juv', flow), x)
+    set_instream_habitat('Butte Creek', 'fr', 'fry', fry_flow), fryx)
+  expect_equal(
+    set_instream_habitat('Butte Creek', 'fr', 'juv', juv_flow), juvx)
+  expect_equal(
+    set_spawning_habitat('Butte Creek', 'fr', spawn_flow), spawnx)
+})
 
+test_that('FR floodplain Butte Creek works', {
+  first_flood_index <-  which(cvpiaHabitat::butte_creek_floodplain$FR_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::butte_creek_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::butte_creek_floodplain,flow_cfs == flow)$FR_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('Butte Creek', 'fr', flow)),
+    floodplain,
+    tolerance = .01)
+})
+
+test_that('SR floodplain Butte Creek works', {
+  first_flood_index <-  which(cvpiaHabitat::butte_creek_floodplain$SR_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::butte_creek_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::butte_creek_floodplain,flow_cfs == flow)$SR_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('Butte Creek', 'sr', flow)),
+    floodplain,
+    tolerance = .01)
+})
+
+test_that('ST floodplain Butte Creek works', {
+  first_flood_index <-  which(cvpiaHabitat::butte_creek_floodplain$ST_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::butte_creek_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::butte_creek_floodplain,flow_cfs == flow)$ST_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('Butte Creek', 'st', flow)),
+    floodplain,
+    tolerance = .01)
+})
+
+# Tests for species without modeling (FALSE modeling_exists) ----
+test_that('SR instream Bear River works', {
+
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_juv_wua))[1]
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_spawn_wua))[1]
+
+  fry_wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[juv_not_na_index]
+  spawn_wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[spawn_not_na_index]
+
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'Butte Creek' & lifestage == 'rearing'
+                                  & species == 'sr')$feet
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Butte Creek' & lifestage == 'spawning'
+                                   & species == 'sr')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[juv_not_na_index]
+  spawn_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[spawn_not_na_index]
+
+  expect_equal(
+    set_instream_habitat('Butte Creek', 'sr', 'fry', fry_flow), fryx)
+  expect_equal(
+    set_instream_habitat('Butte Creek', 'sr', 'juv', juv_flow), juvx)
+  expect_equal(
+    set_spawning_habitat('Butte Creek', 'sr', spawn_flow), spawnx)
+})
+
+test_that('ST instream Bear River works', {
+
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_juv_wua))[1]
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::butte_creek_instream$FR_spawn_wua))[1]
+
+  fry_wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[juv_not_na_index]
+  spawn_wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[spawn_not_na_index]
+
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'Butte Creek' & lifestage == 'rearing'
+                                  & species == 'st')$feet
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Butte Creek' & lifestage == 'spawning'
+                                   & species == 'st')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[juv_not_na_index]
+  spawn_flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[spawn_not_na_index]
+
+  expect_equal(
+    set_instream_habitat('Butte Creek', 'st', 'fry', fry_flow), fryx)
+  expect_equal(
+    set_instream_habitat('Butte Creek', 'st', 'juv', juv_flow), juvx)
+  expect_equal(
+    set_spawning_habitat('Butte Creek', 'st', spawn_flow), spawnx)
 })
 
 
-test_that('SR fry Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'sr', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_instream_habitat('Butte Creek', 'sr', 'fry', flow), x)
-
-})
-
-test_that('SR juv Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'sr', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_instream_habitat('Butte Creek', 'sr', 'juv', flow), x)
-
-})
-
-test_that('ST fry Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_fry_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'st', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_instream_habitat('Butte Creek', 'st', 'fry', flow), x)
-
-})
-
-test_that('ST juv Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_juv_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'rearing' &
-      cvpiaHabitat::watershed_lengths$species == 'st', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_instream_habitat('Butte Creek', 'st', 'juv', flow), x)
-
-})
-
-# spawning
-test_that('FR spawn Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'spawning' &
-      cvpiaHabitat::watershed_lengths$species == 'fr', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_spawning_habitat('Butte Creek', 'fr', flow), x)
-
-})
-
-test_that('SR spawn Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'spawning' &
-      cvpiaHabitat::watershed_lengths$species == 'sr', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_spawning_habitat('Butte Creek', 'sr', flow), x)
-
-})
-
-test_that('ST spawn Butte Creek works', {
-  # flow 100
-  wua <- cvpiaHabitat::butte_creek_instream$FR_spawn_wua[10]
-  stream_length <- as.numeric(cvpiaHabitat::watershed_lengths[
-    cvpiaHabitat::watershed_lengths$watershed == 'Butte Creek' &
-      cvpiaHabitat::watershed_lengths$lifestage == 'spawning' &
-      cvpiaHabitat::watershed_lengths$species == 'st', 'feet'])
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::butte_creek_instream$flow_cfs[10]
-  expect_equal(
-    set_spawning_habitat('Butte Creek', 'st', flow), x)
-
-})
