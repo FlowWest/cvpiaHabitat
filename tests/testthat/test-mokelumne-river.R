c0b29bf3940c5da0d45fb87d98b36b496075d85f
Isabelle Kavanagh <ikavanagh@flowwest.com>
2020-09-17 14:50:42 -0700

    moke and san joaquin testing edits

--- tests/testthat/test-mokelumne-river.R
+++ tests/testthat/test-mokelumne-river.R
@@ -1,64 +1,87 @@
 library(cvpiaHabitat)
 context('Mokelumne River Habitat')
 
-test_that('FR fry Mokelumne River works', {
+test_that("modeling of species coverage hasn't changed since v2.0 - Mokelumne", {
+  modeling <- subset(cvpiaHabitat::modeling_exist, Watershed == 'Mokelumne River')
 
-  wua <- cvpiaHabitat::mokelumne_river_instream$FR_fry_wua[12]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Mokelumne River' & lifestage == 'rearing'
-                          & species == 'fr')$feet
-  x <- ((stream_length/1000) * wua)/10.7639
+  expect_equal(modeling$FR_spawn, TRUE)
+  expect_equal(modeling$FR_fry, TRUE)
+  expect_equal(modeling$FR_juv, TRUE)
+  expect_equal(modeling$FR_floodplain, TRUE)
 
-  flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[12]
-  expect_equal(
-    set_instream_habitat('Mokelumne River', 'fr', 'fry', flow), x)
+  expect_equal(modeling$SR_spawn, FALSE)
+  expect_equal(modeling$SR_fry, FALSE)
+  expect_equal(modeling$SR_juv, FALSE)
+  expect_equal(modeling$SR_floodplain, FALSE)
 
-
+  expect_equal(modeling$ST_spawn, TRUE)
+  expect_equal(modeling$ST_fry, FALSE)
+  expect_equal(modeling$ST_juv, FALSE)
+  expect_equal(modeling$ST_floodplain, FALSE)
+  expect_equal(modeling$ST_adult, FALSE)
 })
 
-test_that('FR juv Mokelumne River works', {
+test_that('FR instream Mokelumne River works', {
 
-  wua <- cvpiaHabitat::mokelumne_river_instream$FR_juv_wua[12]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Mokelumne River' & lifestage == 'rearing'
-                          & species == 'fr')$feet
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::mokelumne_river_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::mokelumne_river_instream$FR_juv_wua))[1]
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::mokelumne_river_instream$FR_spawn_wua))[1]
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  fry_wua <- cvpiaHabitat::mokelumne_river_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::mokelumne_river_instream$FR_juv_wua[juv_not_na_index]
+  spawn_wua <- cvpiaHabitat::mokelumne_river_instream$FR_spawn_wua[spawn_not_na_index]
 
-  flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[12]
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'Mokelumne River' & lifestage == 'rearing'
+                                  & species == 'fr')$feet
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Mokelumne River' & lifestage == 'spawning'
+                                   & species == 'fr')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[juv_not_na_index]
+  spawn_flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[spawn_not_na_index]
+
   expect_equal(
-    set_instream_habitat('Mokelumne River', 'fr', 'juv', flow), x)
-
-})
-
-test_that('FR spawn Mokelumne River works', {
-
-  wua <- cvpiaHabitat::mokelumne_river_instream$FR_spawn_wua[11]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Mokelumne River' & lifestage == 'spawning'
-                          & species == 'fr')$feet
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[11]
+    set_instream_habitat('Mokelumne River', 'fr', 'fry', fry_flow), fryx)
   expect_equal(
-    set_spawning_habitat('Mokelumne River', 'fr', flow), x)
-
+    set_instream_habitat('Mokelumne River', 'fr', 'juv', juv_flow), juvx)
+  expect_equal(
+    set_spawning_habitat('Mokelumne River', 'fr', spawn_flow), spawnx)
 })
 
 test_that('ST spawn Mokelumne River works', {
 
-  wua <- cvpiaHabitat::mokelumne_river_instream$ST_spawn_wua[11]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Mokelumne River' & lifestage == 'spawning'
-                          & species == 'fr')$feet
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::mokelumne_river_instream$ST_spawn_wua))[1]
+  spawn_wua <- cvpiaHabitat::mokelumne_river_instream$ST_spawn_wua[spawn_not_na_index]
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  #TODO - there is no steelhead extent for mokelumne river - is fall run ok? (Issue #205)
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Mokelumne River' & lifestage == 'spawning'
+                                   & species == 'fr')$feet
 
-  flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[11]
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  spawn_flow <- cvpiaHabitat::mokelumne_river_instream$flow_cfs[spawn_not_na_index]
   expect_equal(
-    set_spawning_habitat('Mokelumne River', 'st', flow), x)
+    set_spawning_habitat('Mokelumne River', 'st', spawn_flow), spawnx)
 
+})
+
+test_that('FR floodplain Mokelumne River works', {
+  first_flood_index <-  which(cvpiaHabitat::mokelumne_river_floodplain$FR_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::mokelumne_river_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::mokelumne_river_floodplain,flow_cfs == flow)$FR_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('Mokelumne River', 'fr', flow)),
+    floodplain,
+    tolerance = .01)
 })
 
 test_that('ST and SR rearing is same as FR', {
@@ -73,12 +96,3 @@ test_that('ST and SR rearing is same as FR', {
     set_instream_habitat('Mokelumne River', 'st', 'juv', 1000))
 })
 
-test_that('FR floodplain works', {
-  floodplain <- mokelumne_river_floodplain$FR_floodplain_acres[2]
-  flow <- mokelumne_river_floodplain$flow_cfs[2]
-
-  expect_equal(
-    floodplain / 0.000247105,
-    set_floodplain_habitat('Mokelumne River', 'fr', flow)
-  )
-})
--- tests/testthat/test-san-joaquin-river.R
+++ tests/testthat/test-san-joaquin-river.R
@@ -1,6 +1,26 @@
 library(cvpiaHabitat)
 context('San Joaquin River Habitat')
 
+test_that("modeling of species coverage hasn't changed since v2.0 - San Joaquin", {
+  modeling <- subset(cvpiaHabitat::modeling_exist, Watershed == 'San Joaquin River')
+
+  expect_equal(modeling$FR_spawn, FALSE)
+  expect_equal(modeling$FR_fry, FALSE)
+  expect_equal(modeling$FR_juv, FALSE)
+  expect_equal(modeling$FR_floodplain, TRUE)
+
+  expect_equal(is.na(modeling$SR_spawn), TRUE)
+  expect_equal(modeling$SR_fry, FALSE)
+  expect_equal(modeling$SR_juv, FALSE)
+  expect_equal(modeling$SR_floodplain, FALSE)
+
+  expect_equal(is.na(modeling$ST_spawn), TRUE)
+  expect_equal(modeling$ST_fry, FALSE)
+  expect_equal(modeling$ST_juv, FALSE)
+  expect_equal(modeling$ST_floodplain, FALSE)
+  expect_equal(modeling$ST_adult, FALSE)
+})
+
 test_that('FR fry San Joaquin River works', {
 
   wua <- cvpiaHabitat::san_joaquin_river_instream$FR_fry_wua[10]
@@ -58,4 +78,42 @@ test_that('ST and SR = FR San Joaquin River works', {
     set_instream_habitat('San Joaquin River', 'sr', 'juv', 1000),
     set_instream_habitat('San Joaquin River', 'st', 'juv', 1000))
 
+})
+
+test_that('FR floodplain San Joaquin River works', {
+  first_flood_index <-  which(cvpiaHabitat::san_joaquin_river_floodplain$FR_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::san_joaquin_river_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::san_joaquin_river_floodplain,flow_cfs == flow)$FR_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('San Joaquin River', 'fr', flow)),
+    floodplain,
+    tolerance = .01)
+})
+
+# Tests for species/habitat without modeling (FALSE modeling_exists) ----
+
+test_that('FR rearing San Joaquin River works', {
+
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::san_joaquin_river_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::san_joaquin_river_instream$FR_juv_wua))[1]
+
+  fry_wua <- cvpiaHabitat::san_joaquin_river_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::san_joaquin_river_instream$FR_juv_wua[juv_not_na_index]
+
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'San Joaquin River' & lifestage == 'rearing'
+                                  & species == 'fr')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::san_joaquin_river_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::san_joaquin_river_instream$flow_cfs[juv_not_na_index]
+
+  expect_equal(
+    set_instream_habitat('San Joaquin River', 'fr', 'fry', fry_flow), fryx)
+  expect_equal(
+    set_instream_habitat('San Joaquin River', 'fr', 'juv', juv_flow), juvx)
 })
