43a036628655b58fa7bc5760a191b655e77e6e2c
Isabelle Kavanagh <ikavanagh@flowwest.com>
2020-09-17 11:34:14 -0700

    feather river testing edits

--- tests/testthat/test-feather-river.R
+++ tests/testthat/test-feather-river.R
@@ -1,64 +1,88 @@
 library(cvpiaHabitat)
 context('Feather River Habitat')
 
-test_that('FR fry Feather River works', {
+test_that("modeling of species coverage hasn't changed since v2.0 - Feather", {
+  modeling <- subset(cvpiaHabitat::modeling_exist, Watershed == 'Feather River')
 
-  wua <- cvpiaHabitat::feather_river_instream$FR_fry_wua[9]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Feather River' & lifestage == 'rearing'
-                          & species == 'fr')$feet
-  x <- ((stream_length/1000) * wua)/10.7639
+  expect_equal(modeling$FR_spawn, TRUE)
+  expect_equal(modeling$FR_fry, TRUE)
+  expect_equal(modeling$FR_juv, TRUE)
+  expect_equal(modeling$FR_floodplain, TRUE)
 
-  flow <- cvpiaHabitat::feather_river_instream$flow_cfs[9]
-  expect_equal(
-    set_instream_habitat('Feather River', 'fr', 'fry', flow), x)
+  expect_equal(modeling$SR_spawn, FALSE)
+  expect_equal(modeling$SR_fry, FALSE)
+  expect_equal(modeling$SR_juv, FALSE)
+  expect_equal(modeling$SR_floodplain, FALSE)
 
-
+  expect_equal(modeling$ST_spawn, TRUE)
+  expect_equal(modeling$ST_fry, FALSE)
+  expect_equal(modeling$ST_juv, FALSE)
+  expect_equal(modeling$ST_floodplain, FALSE)
+  expect_equal(modeling$ST_adult, FALSE)
 })
 
-test_that('FR juv Feather River works', {
+test_that('FR instream Feather River works', {
 
-  wua <- cvpiaHabitat::feather_river_instream$FR_juv_wua[9]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Feather River' & lifestage == 'rearing'
-                          & species == 'fr')$feet
+  fry_not_na_index <- which(!is.na(cvpiaHabitat::feather_river_instream$FR_fry_wua))[1]
+  juv_not_na_index <- which(!is.na(cvpiaHabitat::feather_river_instream$FR_juv_wua))[1]
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::feather_river_instream$FR_spawn_wua))[1]
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  fry_wua <- cvpiaHabitat::feather_river_instream$FR_fry_wua[fry_not_na_index]
+  juv_wua <- cvpiaHabitat::feather_river_instream$FR_juv_wua[juv_not_na_index]
+  spawn_wua <- cvpiaHabitat::feather_river_instream$FR_spawn_wua[spawn_not_na_index]
 
-  flow <- cvpiaHabitat::feather_river_instream$flow_cfs[9]
+  rearing_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                  watershed == 'Feather River' & lifestage == 'rearing'
+                                  & species == 'fr')$feet
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Feather River' & lifestage == 'spawning'
+                                   & species == 'fr')$feet
+
+  fryx <- (((rearing_stream_length/1000) * fry_wua)/10.7639)
+  juvx <- (((rearing_stream_length/1000) * juv_wua)/10.7639)
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  fry_flow <- cvpiaHabitat::feather_river_instream$flow_cfs[fry_not_na_index]
+  juv_flow <- cvpiaHabitat::feather_river_instream$flow_cfs[juv_not_na_index]
+  spawn_flow <- cvpiaHabitat::feather_river_instream$flow_cfs[spawn_not_na_index]
+
   expect_equal(
-    set_instream_habitat('Feather River', 'fr', 'juv', flow), x)
-
+    set_instream_habitat('Feather River', 'fr', 'fry', fry_flow), fryx)
+  expect_equal(
+    set_instream_habitat('Feather River', 'fr', 'juv', juv_flow), juvx)
+  expect_equal(
+    set_spawning_habitat('Feather River', 'fr', spawn_flow), spawnx)
 })
 
-test_that('FR spawn Feather River works', {
-
-  wua <- cvpiaHabitat::feather_river_instream$FR_spawn_wua[9]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Feather River' & lifestage == 'spawning'
-                          & species == 'fr')$feet
-
-  x <- ((stream_length/1000) * wua)/10.7639
-
-  flow <- cvpiaHabitat::feather_river_instream$flow_cfs[9]
-  expect_equal(
-    set_spawning_habitat('Feather River', 'fr', flow), x)
-
-})
 
 test_that('ST spawn Feather River works', {
 
-  wua <- cvpiaHabitat::feather_river_instream$ST_spawn_wua[9]
-  stream_length <- subset(cvpiaHabitat::watershed_lengths,
-                          watershed == 'Feather River' & lifestage == 'spawning'
-                          & species == 'fr')$feet
+  spawn_not_na_index <- which(!is.na(cvpiaHabitat::feather_river_instream$ST_spawn_wua))[1]
+  spawn_wua <- cvpiaHabitat::feather_river_instream$ST_spawn_wua[spawn_not_na_index]
 
-  x <- ((stream_length/1000) * wua)/10.7639
+  #TODO - there is no steelhead extent for feather river - is fall run ok? (Issue #205)
+  spawning_stream_length <- subset(cvpiaHabitat::watershed_lengths,
+                                   watershed == 'Feather River' & lifestage == 'spawning'
+                                   & species == 'fr')$feet
 
-  flow <- cvpiaHabitat::feather_river_instream$flow_cfs[9]
+  spawnx <- (((spawning_stream_length/1000) * spawn_wua)/10.7639)
+
+  spawn_flow <- cvpiaHabitat::feather_river_instream$flow_cfs[spawn_not_na_index]
   expect_equal(
-    set_spawning_habitat('Feather River', 'st', flow), x)
+    set_spawning_habitat('Feather River', 'st', spawn_flow), spawnx)
 
+})
+
+test_that('FR floodplain Feather River works', {
+  first_flood_index <-  which(cvpiaHabitat::feather_river_floodplain$FR_floodplain_acres > 0)[1]
+
+  flow <- cvpiaHabitat::feather_river_floodplain$flow_cfs[first_flood_index]
+  floodplain <- subset(cvpiaHabitat::feather_river_floodplain,flow_cfs == flow)$FR_floodplain_acres
+
+  expect_equal(
+    square_meters_to_acres(set_floodplain_habitat('Feather River', 'fr', flow)),
+    floodplain,
+    tolerance = .01)
 })
 
 test_that('SR and FR are same for spawning', {
